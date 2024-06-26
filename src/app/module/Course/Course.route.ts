7ed1e2d6e4f34c8daf9398177644fe2574ca4e6e
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-06-26 22:24:03 +0600

    authmiddle course module

--- src/app/module/Course/Course.route.ts
+++ src/app/module/Course/Course.route.ts
@@ -2,26 +2,38 @@ import express from "express";
 import validationMiddleWare from "../../middleware/ZodSchemaValidationMiddleware";
 import { CourseZodValidations } from "./Course.zodValidation";
 import { courseController } from "./Course.controller";
+import { authMiddleWare } from "../../middleware/AuthMiddleWare";
+import { USER_ROLE } from "../User/User.const";
 
 const router = express.Router();
 
 router.post(
   "/create-course",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(CourseZodValidations.createCourseValidationSchemaZod),
   courseController.createCourse
 );
 router.patch(
   "/:id",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(CourseZodValidations.updateCourseValidationSchemaZod),
   courseController.updateCourse
 );
-router.get("/", courseController.findAllCourse);
-router.get("/:id", courseController.findOneCourse);
-router.delete("/:id", courseController.deleteCourse);
+router.get(
+  "/",
+  authMiddleWare(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+  courseController.findAllCourse
+);
+router.delete(
+  "/:id",
+  authMiddleWare(USER_ROLE.admin),
+  courseController.deleteCourse
+);
 
 // faculties courses
 router.put(
   "/:courseId/assign-faculty",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(
     CourseZodValidations.facultiesWithCourseValidationSchemaZod
   ),
@@ -29,6 +41,7 @@ router.put(
 );
 router.delete(
   "/:courseId/remove-faculty",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(
     CourseZodValidations.facultiesWithCourseValidationSchemaZod
   ),
