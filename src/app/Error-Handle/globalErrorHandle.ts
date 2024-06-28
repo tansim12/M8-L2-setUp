bfd5ddd3d8e462b17f22baadf650e649d3326c53
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-06-28 20:46:47 +0600

    global error handlar

--- src/app/Error-Handle/globalErrorHandle.ts
+++ src/app/Error-Handle/globalErrorHandle.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodError, ZodIssue } from "zod";
+import { ZodError } from "zod";
 import { TerrorSources } from "../Interface/error";
 import handleZodError from "./handleZodError";
 
@@ -25,37 +25,36 @@ const globalErrorHandler = (
     },
   ];
 
-  console.log(err);
 
   if (err instanceof ZodError) {
     const simplifyError = handleZodError(err);
 
-    (statusCode = simplifyError?.statusCode),
-      (message = simplifyError?.message),
+    (statusCode = simplifyError?.statusCode);
+      (message = simplifyError?.message);
       (errorSources = simplifyError?.errorSources);
-  } else if (err.name === "ValidationError") {
+  } else if (err?.name === "ValidationError") {
     const simplifyError = handleValidationError(err);
 
-    (statusCode = simplifyError?.statusCode),
-      (message = simplifyError?.message),
+    (statusCode = simplifyError?.statusCode);
+      (message = simplifyError?.message);
       (errorSources = simplifyError?.errorSources);
   }
-  else if (err.name === "CastError") {
+  else if (err?.name === "CastError") {
     const simplifyError = handleCastError(err);
-    (statusCode = simplifyError?.statusCode),
-      (message = simplifyError?.message),
+    (statusCode = simplifyError?.statusCode);
+      (message = simplifyError?.message);
       (errorSources = simplifyError?.errorSources);
   }
-  else if (err.name === "CastError") {
+  else if (err?.name === 'CastError') {
     const simplifyError = handleCastError(err);
-    (statusCode = simplifyError?.statusCode),
-      (message = simplifyError?.message),
-      (errorSources = simplifyError?.errorSources);
+    statusCode = simplifyError?.statusCode;
+    message = simplifyError?.message;
+    errorSources = simplifyError?.errorSources;
   }
-  else if (err.code === 11000) {
+  else if (err?.code === 11000) {
     const simplifyError = handleDuplicateError(err);
-    (statusCode = simplifyError?.statusCode),
-      (message = simplifyError?.message),
+    (statusCode = simplifyError?.statusCode);
+      (message = simplifyError?.message);
       (errorSources = simplifyError?.errorSources);
   }
 
@@ -88,15 +87,3 @@ const globalErrorHandler = (
 
 export default globalErrorHandler;
 
-// throw new error pattern
-
-/**
- * success
- * message
- * errorSources:[
- * path:"",
- * message:""]
- * stack:
- *
- * ]
- */
