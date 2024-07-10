4501d2311aaac896b36a92fb7e7c1191923ad461
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-07-10 18:43:44 +0600

    handle when decoded function some error

--- src/app/module/Semester/Semester.route.ts
+++ src/app/module/Semester/Semester.route.ts
@@ -2,22 +2,31 @@ import express from "express";
 import { semesterController } from "./Semester.controller";
 import validationMiddleWare from "../../middleware/ZodSchemaValidationMiddleware";
 import { semesterZodValidation } from "./Semester.zodValidation";
+import { authMiddleWare } from "../../middleware/AuthMiddleWare";
+import { USER_ROLE } from "../User/User.const";
 
 const router = express.Router();
 
 router.post(
   "/create-academic-semester",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(semesterZodValidation.SemesterSchemaZod),
   semesterController.createSemester
 );
 router.get(
-  "/create-academic-semester",
+  "/",
+  authMiddleWare(USER_ROLE.admin),
 
   semesterController.findAllSemester
 );
-router.get("/:semesterId", semesterController.getOneSemester);
+router.get(
+  "/:semesterId",
+  authMiddleWare(USER_ROLE.admin),
+  semesterController.getOneSemester
+);
 router.patch(
   "/:semesterId",
+  authMiddleWare(USER_ROLE.admin),
   validationMiddleWare(semesterZodValidation.UpdateSemesterSchemaZod),
   semesterController.updateSemesterData
 );
